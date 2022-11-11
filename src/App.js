60b0e61657f8e45a4f19fce69d138607de8d74d5
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-11-11 00:03:32 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -21,35 +21,31 @@ function App() {
         }
       })
       .then((data) => {
+        console.log(data)
         setWeatherForecast(data)
       })
   }
 
   return (
     <div>
-      <h1>Weather API</h1>
+  
       <nav className="navbar navbar-expadn-md navbar-dark bg-dark mb-4">
-        <a className="navbar-brand text-white" href="top">
-          Previsão do tempo
+        <a className="navbar-brand text-white" href="weatherforecast">
+          PREVISÃO DO TEMPO
         </a>
-        <a className="navbar-brand text-white" href="top">
-          Previsão do tempo
-        </a>
-        <a className="navbar-brand text-white" href="top">
-          Previsão do tempo
-        </a>
+  
       </nav>
 
       <main className="container">
         <div className="p-5 mb-5 bg-light rounded-3">
-          <h1>Previsão do tempo de sua cidade</h1>
+          <h1>Previsão do tempo atual</h1>
           <p>
-            Digite o nome da sua cidade no campo abaixo e em seguida clique em pesquisar
+            Digite o nome da cidade:
           </p>
 
           <div className="row mb-4">
             <div className="col-md-6">
-              <input onChange={handleChange} className="form-control" value={city} />
+              <input onChange={handleChange} placeholder='Ex: Brasilia, Rio de Janeiro, São Paulo' className="form-control" value={city} />
             </div>
 
           </div>
@@ -63,9 +59,12 @@ function App() {
                     <img src={weatherForecast.current.condition.icon} />
                   </div>
                   <div>
-                  <h3>Hoje o dia está: {weatherForecast.current.condition.text} </h3>
+                  <h3>Clima: {weatherForecast.current.condition.text} </h3>
                   <p className='lead'>
-                    Temp: {weatherForecast.current.temp_c}
+                    Temperatura: {weatherForecast.current.temp_c}
+                  </p>
+                  <p className='lead'>
+                    Sensação: {weatherForecast.current.feelslike_c}
                   </p>
 
                   </div>
